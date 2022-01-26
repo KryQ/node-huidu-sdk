960a7f59130b9d9f757ca4198934fee5e0bdd6e0
Krystian D <krystian.dolatowski@3d.com.pl>
2022-01-26 10:27:52 +0100

    switched from throws to reject in tcpqueue

--- src/helpers/TcpQueue.ts
+++ src/helpers/TcpQueue.ts
@@ -1,5 +1,6 @@
+import { ErrorCode } from "../utils/ReturnCodes";
 class TcpQueue {
-    private _queue: {
+	private _queue: {
         [key: string]: {
             resolve: any;
             reject: any,
@@ -7,37 +8,37 @@ class TcpQueue {
         }
     } = {};
 
-    push = (cmd: string, reject: any, resolve: any, timeout: number = 2000): void => {
-        const entry = Object.entries(this._queue).find(x => x[0] === cmd);
+	push = (cmd: string, reject: any, resolve: any, timeout = 2000): void => {
+		const entry = Object.entries(this._queue).find(x => x[0] === cmd);
 
-        if (entry) {
-            throw new Error("Request is already pending");
-        }
-        else {
-            const timeout_handle = setTimeout(() => {
-                this.read(cmd);
-                reject(`timeout: ${cmd}`)
-            }, timeout);
+		if (entry) {
+			reject(ErrorCode.REQUEST_PENDING);
+		}
+		else {
+			const timeout_handle = setTimeout(() => {
+				this.read(cmd);
+				reject(`timeout: ${cmd}`);
+			}, timeout);
 
-            Object.assign(this._queue, { [cmd]: { reject: reject, resolve: resolve, timeout: timeout_handle } })
-        }
-    }
+			Object.assign(this._queue, { [cmd]: { reject: reject, resolve: resolve, timeout: timeout_handle } });
+		}
+	};
 
-    read = (cmd: string): any => {
-        const obj = Object.entries(this._queue).find(x => x[0] === cmd);
-        if (obj) {
-            clearTimeout(obj[1].timeout)
-            delete this._queue[cmd];
+	read = (cmd: string): any => {
+		const obj = Object.entries(this._queue).find(x => x[0] === cmd);
+		if (obj) {
+			clearTimeout(obj[1].timeout);
+			delete this._queue[cmd];
 
-            return obj[1];
-        }
+			return obj[1];
+		}
 
-        return undefined;
-    }
+		return undefined;
+	};
 
-    print = (): string => {
-        return JSON.stringify(Object.keys(this._queue));
-    }
+	print = (): string => {
+		return JSON.stringify(Object.keys(this._queue));
+	};
 }
 
-export { TcpQueue }
+export { TcpQueue };
