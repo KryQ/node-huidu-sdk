49f7418bb7136bb449e11b46c266e3bf26821e94
Krystian D <krystian.dolatowski@3d.com.pl>
2022-02-01 14:23:14 +0100

    export commstate fix

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,10 @@
-import { DisplayCommunicator } from "./DisplayCommunicator.js";
-import  DisplayDevice  from "./DisplayDevice.js";
+import { DisplayCommunicator, ConnectionState } from "./DisplayCommunicator.js";
+import DisplayDevice from "./DisplayDevice.js";
 import {Program} from "./ProgramPlanner/Program.js";
 import BaseComponent from "./ProgramPlanner/BaseComponent.js";
 import ImageComponent from "./ProgramPlanner/ImageComponent.js";
 import ParkingSpacesComponent from "./ProgramPlanner/ParkingSpacesComponent.js";
 import TextComponent from "./ProgramPlanner/TextComponent.js";
 import { DeviceFile } from "./utils/Types.js";
-import { ConnectionState } from "./DisplayCommunicator.js";
 
 export { DisplayCommunicator, DisplayDevice, Program, BaseComponent, ImageComponent, ParkingSpacesComponent, TextComponent, DeviceFile, ConnectionState };
