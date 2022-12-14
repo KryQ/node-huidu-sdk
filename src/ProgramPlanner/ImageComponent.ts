08b1f6e2b815bca56727d32621aed69c49f29bf3
KryQ <krystian.dolatowski@rspective.pl>
2022-12-14 18:40:51 +0100

    0.0.29
    forced image name as mandatory

--- src/ProgramPlanner/ImageComponent.ts
+++ src/ProgramPlanner/ImageComponent.ts
@@ -1,17 +1,17 @@
 import BaseComponent from "./BaseComponent.js";
 
-type TImage = { name?: string, md5?: string };
+type TImage = { name: string, md5?: string };
 
 class ImageComponent extends BaseComponent {
 	readonly type = "image";
 
-	name?: string;
+	name: string;
 	md5?: string;
 
 	constructor(x: number, y: number, width: number, height: number, alpha: number, image: TImage, guid?: string) {
 		super(x, y, width, height, alpha, guid);
 
-		if (image.name === undefined && image.md5 === undefined) {
+		if (image.name === undefined) {
 			throw new Error("Component need either name or md5 hash of image!");
 		}
 
@@ -20,7 +20,7 @@ class ImageComponent extends BaseComponent {
 	}
 
 	setImage = (image: TImage) => {
-		if (image.name === undefined && image.md5 === undefined) {
+		if (image.name === undefined) {
 			throw new Error("Component need either name or md5 hash of image!");
 		}
 
@@ -28,28 +28,26 @@ class ImageComponent extends BaseComponent {
 		this.md5 = image.md5;
 	};
 
-	generate = (): object => {
-		return {
-			"@_alpha": this.alpha,
-			"@_guid": this.guid,
-			"rectangle": {
-				"@_x": this.x,
-				"@_height": this.height,
-				"@_width": this.width,
-				"@_y": this.y
-			},
-			"resources": {
-				"image": {
-					"@_guid": this.guid + "Image",
-					"@_fit": "fill",
-					"file": {
-						...(this.name && !this.md5 && { "@_name": this.name }),
-						...(this.md5 && { "@_md5": this.md5 }),
-					}
+	generate = (): object => ({
+		"@_alpha": this.alpha,
+		"@_guid": this.guid,
+		"rectangle": {
+			"@_x": this.x,
+			"@_height": this.height,
+			"@_width": this.width,
+			"@_y": this.y
+		},
+		"resources": {
+			"image": {
+				"@_guid": this.guid + "Image",
+				"@_fit": "fill",
+				"file": {
+					"@_name": this.name,
+					...(this.md5 && { "@_md5": this.md5 }),
 				}
 			}
-		};
-	};
+		}
+	});
 }
 
 export default ImageComponent;
